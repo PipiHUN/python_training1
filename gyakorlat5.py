15a5c59fba121da03e014138b8875479af208872
T360-v2-PYT2-s04 <jotvos85@gmail.com>
2021-10-13 16:27:23 +0200

    gyakorlat 5

---
+++ gyakorlat5.py
@@ -0,0 +1,19 @@
+#szorzás
+line = ""
+i = 1
+while i <= 20:
+    result = i * 7
+    if result % 3 == 0:
+        print += (str(result) + " * ")
+    else:
+        print += (str(result) + " ")
+    i = i +1
+Print (line)
+
+#karifa
+csillag = "*"
+i = 1
+while i <= 7:
+    print(csillag)
+    csillag = csillag + "*"
+    i = i +1
