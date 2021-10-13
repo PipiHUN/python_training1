fdd2e7fdb622ec5101219934ccd6095e5c281d34
Résztvevő - 4 <T360-v2-PYT2-s04@oktatas.local>
2021-10-13 15:35:54 +0200

    Initial commit

---
+++ sum_numbers.py
@@ -0,0 +1,7 @@
+# Ez a program bekér két számot
+
+a_input = input("Add emg az első számot!") #Bekéri az első számot
+b_input = input("Írd be a második számot")
+a=int(a_input) #Konvertálás
+b=int(b_input)
+print(a+b)
