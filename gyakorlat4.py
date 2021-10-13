e1ad95f65396e3d01cbafa98df7f2389071dbb3b
T360-v2-PYT2-s04 <jotvos85@gmail.com>
2021-10-13 16:16:21 +0200

    x

---
+++ gyakorlat4.py
@@ -0,0 +1,29 @@
+#i = 1
+#while i <= 20:
+#   print(i*7)
+#    i = i + 1
+
+
+#eur = 1
+#exchange_rate = 1.65
+#max_eur = 16385
+#while eur < max_eur:
+#    print(eur, "euro", exchange_rate, "dollar")
+#    eur *= 2
+
+#a, b = 1, 1
+#while a <= 12:
+#   if a % 2:
+#       b = b * 3
+#       print(b,a)
+#   else:
+#       print(b,a)
+#   a=a+b
+
+#karifa
+csillag = "*"
+i = 1
+while i <= 7:
+    print(csillag)
+    csillag = csillag + "*"
+    i = i +1
